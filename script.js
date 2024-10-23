0ccc09c52c6982b8110515cbe16ef1ed25367faf
ajaykv05 <ajayvishu7741@gmail.com>
2024-10-23 19:45:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function ShowMenu(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex'
+}
+function HideMenu(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none'
+
+}
